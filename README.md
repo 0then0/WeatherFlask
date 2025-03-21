df6dcd73eda8bca7c52c3f8535ce7e7dfe33cd94
0then0 <zheltov_pe@mail.ru>
2025-03-21 18:52:16 +0300

    Add city definition by given coordinates

--- README.md
+++ README.md
@@ -26,7 +26,7 @@ This is a simple REST API built with Flask that fetches weather data from the Op
 2. Create a virtual environment and activate it:
    ```sh
    python -m venv .venv
-   source venv/bin/activate  # On Windows use: venv\Scripts\activate
+   source venv/bin/activate  # On Windows use: .venv\Scripts\activate
    ```
 3. Install dependencies:
    ```sh
@@ -50,10 +50,12 @@ Example response:
 
 ```json
 {
-	"Coordinates": "Lat: 37.7632°N, Lon: -122.4128°E",
-	"Current apparent temperature": 3.43,
-	"Current temperature": 6.50,
-	"Current weather code": 3,
-	"Current wind speed": 9.69
+	"City": "San Francisco",
+	"Coordinates": "Lat: 37.7632°N Lon: -122.4128°E",
+	"Current apparent temperature": 6.91,
+	"Current temperature": 8.5,
+	"Current weather code": 0,
+	"Current wind speed": 3.75,
+	"Last update": "2025-03-21 18:30:00"
 }
 ```
--- app/utils/open_meteo.py
+++ app/utils/open_meteo.py
@@ -1,11 +1,31 @@
-import openmeteo_requests
+from datetime import datetime
 
+import openmeteo_requests
 import requests_cache
+from geopy.geocoders import Nominatim
 from retry_requests import retry
 
-import requests
-
 from app.config import Config
+
+geolocator = Nominatim(user_agent="WeatherFlask")
+
+
+def get_city_name(lat, lon):
+    """
+    Fetch city name from coordinates using geopy.
+
+    :param lat: Latitude
+    :param lon: Longitude
+    :return: City name as string or 'Location not found' if not found
+    """
+    location = geolocator.reverse((lat, lon), language="en")
+
+    if location:
+        address = location.raw["address"]
+        return address.get("city") or address.get("town") or address.get("village")
+
+    return "Location not found"
+
 
 def get_weather(lat, lon):
     """
@@ -13,36 +33,44 @@ def get_weather(lat, lon):
 
     :param lat: Latitude of the location
     :param lon: Longitude of the location
-    :return: Dictionary with weather data or error message
+    :return: Dictionary with weather data
     """
-    try:
-        # Setup the Open-Meteo API client with cache and retry on error
-        cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
-        retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
-        openmeteo = openmeteo_requests.Client(session = retry_session)
-        
-        params = {
-            "latitude": lat,
-            "longitude": lon,
-            "current": ["apparent_temperature", "temperature_2m", "weather_code", "wind_speed_10m"],
-        }
-        responses = openmeteo.weather_api(Config.OPEN_METEO_API_URL, params=params)
-        
-        response = responses[0]
-        current = response.Current()
-        
-        current_apparent_temperature = current.Variables(0).Value()
-        current_temperature_2m = current.Variables(1).Value()
-        current_weather_code = current.Variables(2).Value()
-        current_wind_speed_10m = current.Variables(3).Value()
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession(".cache", expire_after=3600)
+    retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    openmeteo = openmeteo_requests.Client(session=retry_session)
 
-        return {
-            "Coordinates": f"Lat: {response.Latitude()}°N, Lon: {response.Longitude()}°E",
-            "Current temperature": current_temperature_2m,
-            "Current apparent temperature": current_apparent_temperature,
-            "Current wind speed": current_wind_speed_10m,
-            "Current weather code": current_weather_code,
-        }
-    
-    except requests.exceptions.RequestException as e:
-        return {"error": str(e)}
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "current": [
+            "apparent_temperature",
+            "temperature_2m",
+            "weather_code",
+            "wind_speed_10m",
+        ],
+    }
+    responses = openmeteo.weather_api(Config.OPEN_METEO_API_URL, params=params)
+
+    response = responses[0]
+    current = response.Current()
+
+    city_name = get_city_name(lat, lon)
+    current_apparent_temperature = current.Variables(0).Value()
+    current_temperature_2m = current.Variables(1).Value()
+    current_weather_code = current.Variables(2).Value()
+    current_wind_speed_10m = current.Variables(3).Value()
+    current_time_iso = current.Time()
+    current_time_formatted = datetime.fromtimestamp(current_time_iso).strftime(
+        "%Y-%m-%d %H:%M:%S"
+    )
+
+    return {
+        "Coordinates": f"Lat: {response.Latitude()}°N Lon: {response.Longitude()}°E",
+        "City": city_name,
+        "Current temperature": current_temperature_2m,
+        "Current apparent temperature": current_apparent_temperature,
+        "Current wind speed": current_wind_speed_10m,
+        "Current weather code": current_weather_code,
+        "Last update": current_time_formatted,
+    }
